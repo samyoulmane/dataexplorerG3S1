ddf45ff1a1ad053a8bfbed7da20a098e8ef56545
Samy Oulmane <43752644+samyoulmane@users.noreply.github.com>
2019-01-18 18:38:43 +0100

    Update global.R

--- global.R
+++ global.R
@@ -1,5 +1,5 @@
 # Script pou définir les éléments communs à l'UI et au serveur
-Sys.setlocale(category = "LC_ALL", locale = "fr_FR.UTF-8")
+Sys.setlocale(category = "LC_ALL", locale = "fr_FR.UTF-8") # Encodage
 # Packages ####
 
 library(shiny)
